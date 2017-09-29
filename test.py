ba4ab8a03bb1c455743ca2054ef55b92b57e0ecd
claudio-walser <walsercl@gmx.ch>
2017-09-29 15:18:12 +0200

    testing for the game

--- test.py
+++ test.py
@@ -1,15 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import time
 from pymouse import PyMouse
+import midi
 from pprint import pprint
 import sys
+
+
+#pattern = midi.read_midifile('/home/claudio/Downloads/EspanjaPrelude.mid')
+# pattern = midi.read_midifile('/home/claudio/Development/python-midi/mary.mid')
+
+# events = []
+# for track in pattern:
+#   for event in track:
+#     if isinstance(event, midi.events.NoteOnEvent):
+#       print(type(event))
+#       events.append(event)
+# events.sort()
+# pprint(events)
+# sys.exit()
+
+
+mouseButton = 1 # 1 for left 2 for right click
 
 mouse = PyMouse()
 screenSize = mouse.screen_size()
 
-scaleFactor = 100
-scaledMaxValue = int(screenSize[1] / scaleFactor)
+scaleFactor = 32
+scaledMaxValue = int(screenSize[1] / scaleFactor + 1)
 pause = 1
 
 for i in range(0, scaledMaxValue):
